e54f8ac8361c24a390ccc9705649ee7d38940cca
jodelajo <jodelajo@gmail.com>
2021-11-24 16:06:39 +0100

    clean up

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,13 @@
 import React from "react";
 import styles from "./Home.module.css";
 import Flame from "../../components/flame/Flame";
+import Title from "../../components/title/Title";
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
-     <div className={styles.flame}>
-     <Flame/>
-     </div>
-      
-      <div className={styles.text}>
-        <p className={styles.title}>Message in a</p>
-        <p className={styles.lighter}>lighter</p>
-      </div>
+      <Flame />
+      <Title />
     </div>
   );
 }
